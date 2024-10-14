72da3bce39a637f8767f9a4e6227313eeb7954d3
leukas <tschtschtsch@gmail.com>
2024-10-14 13:30:25 +0200

    evaluate + ipynb done

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # BabyLM-Tiny
 A small version of BabyLM for Bachelor projects.
+
+
+
+
+### Usage
+
+##### main.ipynb
+A template for you to fill in. Contains preprocessing code for the dataset to help you get started.
+
+
+
+##### evaluate_blimp.py
+Evaluates an encoder or decoder model on BLiMP. Only evaluates some test models (BERT-tiny and Tiny-GPT2), so you will have to adjust the code to evaluate your own model.
+
+To run, call:
+```
+python evaluate_blimp.py \
+    --model_type [encoder/decoder] \
+    --batch_size 64 
+```
+
+The batch size will not affect the results, it can be raised or lowered based on GPU RAM.
+
+
+##### evaluate_glue.py
+Evaluates an encoder or decoder model on GLUE. Only evaluates some test models (BERT-tiny and Tiny-GPT2), so you will have to adjust the code to evaluate your own model.
+
+To run, call:
+```
+python evaluate_blimp.py \
+    --model_type [encoder/decoder] \
+    --subset [cola/sst2/mrpc/qnli/rte/boolq/multirc] \
+    --batch_size 64 \
+    --learning_rate 5e-5 \
+    --max_epochs 10 \
+    --patience 3 
+```
+
+Here, the batch size *will* affect the results. GLUE tasks require training, so it will train for a maximum of 10 epochs, and stop early if no improvement after 3 epochs.
---
+++ evaluate_glue.py
@@ -0,0 +1,151 @@
+# evaluate_glue.py
+import argparse
+import datasets
+import torch
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from functools import partial
+from tqdm import tqdm
+
+
+GLUE_SUBSETS = ['cola','sst2', 'mrpc', 'qnli', 'rte', 'boolq', 'multirc']
+INPUTS = {
+    'cola': ['sentence'],
+    'sst2': ['sentence'],
+    'mrpc': ['sentence1', 'sentence2'],
+    'qnli': ['question', 'sentence'],
+    'rte': ['sentence1', 'sentence2'],
+    'boolq': ['question', 'passage'],
+    'multirc': ['paragraph', 'question', 'answer']
+}
+DEVICE = 'cuda'
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--subset', type=str, required=True, choices=GLUE_SUBSETS)
+parser.add_argument('--model_type', type=str, required=True, choices=['encoder', 'decoder'])
+parser.add_argument('--bs', '--batch_size', type=int, default=64)
+parser.add_argument('--lr', '--learning_rate', type=float, default=5e-5)
+parser.add_argument('--max_epochs', type=int, default=10)
+parser.add_argument('--patience', type=int, default=3)
+
+def tokenize(examples, tokenizer, subset, truncate=True):
+    batch = {
+        "input_ids": [],
+        "labels": [],
+    }
+
+    for i in range(len(examples['label'])):
+        input_txt = " ".join([examples[txt][i] for txt in INPUTS[subset]])
+        input_ids = tokenizer.encode(input_txt, truncation=truncate)
+        batch["input_ids"].append(input_ids)
+        batch["labels"].append([examples['label'][i]])
+
+    return batch
+
+def padding_collate_fn(batch, max_len=1024, left_padding=False):
+    """ 
+        Pads each list with zeros and concatenates by key.
+        Input: List[{key: List[], ...}]
+        Output: {key: LongTensor(), ...}
+    """
+    padded_batch = {}
+    for key in batch[0]:
+        largest = min(max_len, max([len(b[key]) for b in batch]))
+        padded_batch[key] = torch.zeros((len(batch), largest), dtype=torch.long)
+        if "labels" in key:
+            padded_batch[key] -= 100
+    
+    for i, sample in enumerate(batch):
+        for key in padded_batch:
+            key_len = min(max_len, len(sample[key]))
+            if left_padding:
+                padded_batch[key][i, -key_len:] = torch.LongTensor(sample[key][:key_len])
+            else:
+                padded_batch[key][i, :key_len] = torch.LongTensor(sample[key][:key_len])
+
+    return padded_batch
+
+
+def main():
+    args = parser.parse_args()
+    model_name = "prajjwal1/bert-tiny" if args.model_type == "encoder" else "sshleifer/tiny-gpt2"
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer.model_max_length = 512 # will truncate the inputs to this length
+    tokenize_fn = partial(tokenize, tokenizer=tokenizer, subset=args.subset) # need to make the function unary for map()
+
+    if args.subset in ['boolq', 'multirc']:
+        dataset = datasets.load_dataset('super_glue', args.subset)
+    else:
+        dataset = datasets.load_dataset('glue', args.subset)
+
+    dataset = dataset.map(tokenize_fn, batched=True, num_proc=4, remove_columns=dataset['train'].column_names) 
+
+    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2).to(DEVICE)
+    model.config.pad_token_id = 0 # NOTE: can remove, only needed for testing tiny-gpt2
+
+    train(model, dataset, args)
+
+
+def train(model, dataset, args):
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
+
+    train_dataloader = torch.utils.data.DataLoader(dataset['train'], batch_size=args.bs, shuffle=True, collate_fn=padding_collate_fn)
+    valid_dataloader = torch.utils.data.DataLoader(dataset['validation'], batch_size=args.bs, shuffle=False, collate_fn=padding_collate_fn)
+
+    best = 0.0
+    patience = args.patience
+    for epoch in range(args.max_epochs):
+        model.train()
+        for batch in tqdm(train_dataloader):
+            inputs = batch['input_ids'].to(device=DEVICE)
+            labels = batch['labels'].to(device=DEVICE)
+            outputs = model(inputs, labels=labels, attention_mask=inputs != 0)
+            loss = outputs.loss
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        model.eval()
+        result = evaluate(model, valid_dataloader, args.subset)
+        print(f"Epoch: {epoch}, Result: {result}")
+
+        if result > best:
+            best = result
+            patience = args.patience
+        else:
+            patience -= 1
+            if patience == 0:
+                break
+
+    print(f"Best result: {best}")
+
+
+def evaluate(model, dataloader, subset):
+    correct = 0.0
+    total = 0.0
+
+    tp, fp, fn = 0.0, 0.0, 0.0
+    with torch.no_grad():
+        for batch in dataloader:
+            input_ids = batch['input_ids'].to(device=DEVICE)
+            labels = batch['labels'][:, 0].to(device=DEVICE)
+
+            outputs = model(input_ids, attention_mask=input_ids != 0).logits.argmax(-1)
+
+            tp += ((outputs == 1) & (labels == 1)).sum().item()
+            fp += ((outputs == 1) & (labels == 0)).sum().item()
+            fn += ((outputs == 0) & (labels == 1)).sum().item()
+            correct += (outputs == labels).sum().item()
+            total += len(labels)
+
+    f1 = 2 * tp / (2 * tp + fp + fn)
+    if subset == 'mrpc':
+        return f1
+    else:
+        return correct / total
+
+
+
+if __name__ == '__main__':
+    main()
