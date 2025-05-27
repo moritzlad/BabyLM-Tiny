46e69609f84adb1a0fa15157c08e64bbb592d333
Moritz <moritz.ladenburger@tum.de>
2025-05-27 12:27:46 +0200

    Add 500k training and validation sets

--- sample_dataset.py
+++ sample_dataset.py
@@ -2,8 +2,9 @@ import random
 import os
 
 TARGET_WORDS = 500_000
+DEV_WORDS = int(0.2 * TARGET_WORDS)
 SNIPPET_SIZE = 10_000
-NUM_SNIPPETS = TARGET_WORDS // SNIPPET_SIZE
+NUM_SNIPPETS = (TARGET_WORDS + DEV_WORDS) // SNIPPET_SIZE
 
 files = os.listdir("./train_10M")
 for file in files:
@@ -12,7 +13,7 @@ for file in files:
 
     total_words = len(words)
 
-    if (total_words > TARGET_WORDS):
+    if (total_words > TARGET_WORDS + DEV_WORDS):
         max_start = total_words - SNIPPET_SIZE
         starts = random.sample(range(max_start), NUM_SNIPPETS)
 
@@ -20,8 +21,13 @@ for file in files:
 
         sampled_words = [word for snippet in snippets for word in snippet]
 
+        train_words = sampled_words[:TARGET_WORDS]
+        dev_words = sampled_words[TARGET_WORDS:]
+
         with open(f"./train_500k/{file}", "w+", encoding="utf-8") as f:
             f.write(" ".join(sampled_words))
+        with open(f"./train_500k/{file[:-6]}_dev.train", "w+", encoding="utf-8") as f:
+            f.write(" ".join(dev_words))
     else: 
         print(f"File {file} has only {total_words} words, not enough to sample {TARGET_WORDS} words.")
 
